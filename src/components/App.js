00365145c8eb0b617ecff33b477da25018822dce
Sahebzad Kushal Deep <kushaldeep448@gmail.com>
2025-05-20 14:38:58 +0530

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,44 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+
+  const [showmodel,setShowmodel]=useState(false)
+
+  const handelshowmodel=()=>{
+    setShowmodel(true)
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <div className="parent">
+          <h1>Parent Component</h1>
+          <Child handelshowmodel={handelshowmodel} showmodel={showmodel}/>
+        </div>
+
     </div>
+  )
+}
+
+const Child = ({handelshowmodel ,showmodel})=>{
+  return (
+    <>
+    <h2>Child component</h2>
+    <button onClick={handelshowmodel}>show model</button>
+
+{
+  showmodel &&(  
+    <div>
+  <h3>Model Content</h3>
+  <p>This is the model content.</p>
+  </div>
+
+  )}
+  </>
+
   )
 }
 
